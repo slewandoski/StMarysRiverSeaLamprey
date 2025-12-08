a29bf0b2d949e2820b51f1ba32c4b8b13cf6f78c
ChrisFishCahill <cahill11@msu.edu>
2025-12-08 12:41:45 -0500

    style file

--- sdmTMB.R
+++ sdmTMB.R
@@ -1,10 +1,10 @@
 # fit some index standardization models using sdmTMB
 #
-# see https://sdmtmb.github.io/sdmTMB/ 
+# see https://sdmtmb.github.io/sdmTMB/
 #
 # note, this takes about 66 minutes to run on my desktop
-# 
-# todo: 
+#
+# todo:
 # - remove type three habitat
 # - model type 1 and 2 habitat as an additional factor
 # - compare abundance predictions among models
@@ -34,23 +34,23 @@ data$c <- data$sl.larv.n
 
 # plot non-zero counts in color, zero in grey
 ggplot() +
-    # zeros first, as faint background
-    geom_point(
-               data  = subset(data, c == 0),
-               aes(X, Y),
-               colour = "grey90",
-               alpha  = 0.95,
-               size   = 0.6
-               ) +
-    # non-zeros on top, with colour scale
-    geom_point(
-               data = subset(data, c > 0),
-               aes(X, Y, colour = c),
-               size = 1
-               ) +
-    coord_fixed() +
-    scale_colour_viridis_c(trans = "asinh") +
-    labs(colour = "count", title = "Surveys with counts > 0")
+  # zeros first, as faint background
+  geom_point(
+    data = subset(data, c == 0),
+    aes(X, Y),
+    colour = "grey90",
+    alpha = 0.95,
+    size = 0.6
+  ) +
+  # non-zeros on top, with colour scale
+  geom_point(
+    data = subset(data, c > 0),
+    aes(X, Y, colour = c),
+    size = 1
+  ) +
+  coord_fixed() +
+  scale_colour_viridis_c(trans = "asinh") +
+  labs(colour = "count", title = "Surveys with counts > 0")
 
 mesh <- make_mesh(data, xy_cols = c("X", "Y"), cutoff = 0.25)
 # mesh$mesh$n
@@ -58,12 +58,12 @@ mesh <- make_mesh(data, xy_cols = c("X", "Y"), cutoff = 0.25)
 
 # no space or year effect
 m1 <- sdmTMB(
-             c ~ s(depth, k = 5), # smoother on depth
-             data = data,
-             mesh = mesh,
-             family = nbinom2(link = "log"),
-             spatial = "off",
-             silent = FALSE
+  c ~ s(depth, k = 5), # smoother on depth
+  data = data,
+  mesh = mesh,
+  family = nbinom2(link = "log"),
+  spatial = "off",
+  silent = FALSE
 )
 
 data$resids <- residuals(m1, type = "mle-mvn") # randomized quantile residuals
@@ -72,12 +72,12 @@ abline(0, 1)
 
 # space effect shared among years
 m2 <- sdmTMB(
-             c ~ 1 + s(depth, k = 5),
-             data = data,
-             mesh = mesh,
-             family = nbinom2(link = "log"),
-             spatial = "on",
-             silent = FALSE
+  c ~ 1 + s(depth, k = 5),
+  data = data,
+  mesh = mesh,
+  family = nbinom2(link = "log"),
+  spatial = "on",
+  silent = FALSE
 )
 
 data$resids <- residuals(m2, type = "mle-mvn") # randomized quantile residuals
@@ -86,94 +86,94 @@ abline(0, 1)
 
 # intercept factor by year, no space or time effects
 m3 <- sdmTMB(
-             c ~ 0 + as.factor(year) + s(depth, k = 5),
-             data = data,
-             mesh = mesh,
-             family = nbinom2(link = "log"),
-             spatial = "off",
-             silent = FALSE
+  c ~ 0 + as.factor(year) + s(depth, k = 5),
+  data = data,
+  mesh = mesh,
+  family = nbinom2(link = "log"),
+  spatial = "off",
+  silent = FALSE
 )
 
 # randomized quantile residuals
-data$resids <- residuals(m3, type = "mle-mvn") 
+data$resids <- residuals(m3, type = "mle-mvn")
 qqnorm(data$resids)
 abline(0, 1)
 
 # intercept factor by year, spatial effect but not time
 m4 <- sdmTMB(
-             c ~ 0 + as.factor(year) + s(depth, k = 5),
-             time = "year",
-             data = data,
-             mesh = mesh,
-             family = nbinom2(link = "log"),
-             spatial = "on",
-             silent = FALSE
+  c ~ 0 + as.factor(year) + s(depth, k = 5),
+  time = "year",
+  data = data,
+  mesh = mesh,
+  family = nbinom2(link = "log"),
+  spatial = "on",
+  silent = FALSE
 )
 
 # one intercept all years, but IID spatial-temporal fields
 m5 <- sdmTMB(
-             c ~ 1 + s(depth, k = 5),
-             time = "year",
-             data = data,
-             mesh = mesh,
-             family = nbinom2(link = "log"),
-             spatial = "on",
-             spatiotemporal = "iid",
-             silent = FALSE
+  c ~ 1 + s(depth, k = 5),
+  time = "year",
+  data = data,
+  mesh = mesh,
+  family = nbinom2(link = "log"),
+  spatial = "on",
+  spatiotemporal = "iid",
+  silent = FALSE
 )
 
 # one intercept all years, but spatiotemporal random walk among years
 m6 <- sdmTMB(
-             c ~ 1 + s(depth, k = 5),
-             time = "year",
-             data = data,
-             mesh = mesh,
-             family = nbinom2(link = "log"),
-             spatial = "on",
-             spatiotemporal = "rw",
-             extra_time = c(1997, 1998, 2020), # ensure regular spacing
-             silent = FALSE
+  c ~ 1 + s(depth, k = 5),
+  time = "year",
+  data = data,
+  mesh = mesh,
+  family = nbinom2(link = "log"),
+  spatial = "on",
+  spatiotemporal = "rw",
+  extra_time = c(1997, 1998, 2020), # ensure regular spacing
+  silent = FALSE
 )
 
 # on intercept all years, spatiotemporal AR1 among years
 m7 <- sdmTMB(
-             c ~ 1 + s(depth, k = 5),
-             time = "year",
-             data = data,
-             mesh = mesh,
-             family = nbinom2(link = "log"),
-             spatial = "on",
-             spatiotemporal = "ar1",
-             extra_time = c(1997, 1998, 2020), # ensure regular spacing
-             silent = FALSE
+  c ~ 1 + s(depth, k = 5),
+  time = "year",
+  data = data,
+  mesh = mesh,
+  family = nbinom2(link = "log"),
+  spatial = "on",
+  spatiotemporal = "ar1",
+  extra_time = c(1997, 1998, 2020), # ensure regular spacing
+  silent = FALSE
 )
 
 # intercept factor by year, spatio-temporal AR1 among years
 data$year_fac <- as.factor(data$year)
 m8 <- sdmTMB(
-             c ~ 0 + year_fac + s(depth, k = 5),
-             time = "year",
-             data = data,
-             mesh = mesh,
-             family = nbinom2(link = "log"),
-             spatial = "on",
-             spatiotemporal = "ar1",
-             extra_time = c(1997, 1998, 2020), # ensure regular spacing
-             silent = FALSE
+  c ~ 0 + year_fac + s(depth, k = 5),
+  time = "year",
+  data = data,
+  mesh = mesh,
+  family = nbinom2(link = "log"),
+  spatial = "on",
+  spatiotemporal = "ar1",
+  extra_time = c(1997, 1998, 2020), # ensure regular spacing
+  silent = FALSE
 )
 
 print(AIC(m1, m2, m3, m4, m5, m6, m7, m8))
 
-# results 
+# results
 #   df      AIC
-#m1  4 31714.61
-#m2  6 28159.21
-#m3 34 28177.78 
-#m4 35 27447.36 fairly "basic" idx standardization model
-#m5  7 27524.55
-#m6  7 27667.27
-#m7  8 27516.50
-#m8 36 27447.02 fairly complicated model, interpolating missing yrs
+# m1  4 31714.61
+# m2  6 28159.21
+# m3 34 28177.78
+# m4 35 27447.36 fairly "basic" idx standardization model
+# m5  7 27524.55
+# m6  7 27667.27
+# m7  8 27516.50
+# m8 36 27447.02 fairly complicated model, interpolating missing yrs
 
 # general plots and diagnostics, can (should) be run for each model
 # m4
@@ -205,17 +205,17 @@ print(AIC(m1, m2, m3, m4, m5, m6, m7, m8))
 # set up a prediction grid -- note this part is interactive to remove
 # a portion of the prediction grid
 
-target_crs   <- 26916   # utm zone 16n, nad83
-grid_spacing <- 100     # meters between grid points
-bbox_buffer  <- 100     # how far beyond survey extent to keep grid (m)
-crop_pad     <- 100     # extra padding around grid+points in plots (m)
+target_crs <- 26916 # utm zone 16n, nad83
+grid_spacing <- 100 # meters between grid points
+bbox_buffer <- 100 # how far beyond survey extent to keep grid (m)
+crop_pad <- 100 # extra padding around grid+points in plots (m)
 
 # ---------------------------
 # read shoreline -> utm 26916
 # ---------------------------
 
-shore_raw   <- st_read("AOC_MI_StMarys_2022/AOC_MI_StMarys_2022.shp")
-shore       <- st_transform(shore_raw, target_crs)
+shore_raw <- st_read("AOC_MI_StMarys_2022/AOC_MI_StMarys_2022.shp")
+shore <- st_transform(shore_raw, target_crs)
 shore_union <- st_union(shore)
 
 # ---------------------------
@@ -230,10 +230,10 @@ dat <- subset(dat, !is.na(longitude) & !is.na(latitude))
 dat_sf <- st_as_sf(dat, coords = c("longitude", "latitude"), crs = 4326)
 dat_sf <- st_transform(dat_sf, target_crs)
 
-coords    <- st_coordinates(dat_sf)
-dat_sf$X  <- coords[, 1]
-dat_sf$Y  <- coords[, 2]
-dat_df    <- st_drop_geometry(dat_sf)
+coords <- st_coordinates(dat_sf)
+dat_sf$X <- coords[, 1]
+dat_sf$Y <- coords[, 2]
+dat_df <- st_drop_geometry(dat_sf)
 
 # ---------------------------
 # bounding box of survey points (utm) with buffer
@@ -251,30 +251,30 @@ ymax_t <- bb["ymax"] + bbox_buffer
 # ---------------------------
 
 grid_all <- st_make_grid(
-                         shore_union,
-                         cellsize = grid_spacing,
-                         what     = "centers"
+  shore_union,
+  cellsize = grid_spacing,
+  what     = "centers"
 )
 
 grid_sf <- st_as_sf(grid_all)
 
 inside_shore <- st_within(grid_sf, shore_union, sparse = FALSE)[, 1]
-grid_sf      <- grid_sf[inside_shore, ]
+grid_sf <- grid_sf[inside_shore, ]
 
 grid_coords <- st_coordinates(grid_sf)
-keep_bbox   <- grid_coords[, 1] >= xmin_t &
-    grid_coords[, 1] <= xmax_t &
-    grid_coords[, 2] >= ymin_t &
-    grid_coords[, 2] <= ymax_t
+keep_bbox <- grid_coords[, 1] >= xmin_t &
+  grid_coords[, 1] <= xmax_t &
+  grid_coords[, 2] >= ymin_t &
+  grid_coords[, 2] <= ymax_t
 
-grid_sf_sub     <- grid_sf[keep_bbox, ]
+grid_sf_sub <- grid_sf[keep_bbox, ]
 grid_coords_sub <- grid_coords[keep_bbox, , drop = FALSE]
 
 pred_grid <- data.frame(
-                        x       = grid_coords_sub[, 1],
-                        y       = grid_coords_sub[, 2],
-                        area_m2 = grid_spacing^2,
-                        area_ha = (grid_spacing^2) / 10000
+  x       = grid_coords_sub[, 1],
+  y       = grid_coords_sub[, 2],
+  area_m2 = grid_spacing^2,
+  area_ha = (grid_spacing^2) / 10000
 )
 
 # ---------------------------
@@ -291,126 +291,132 @@ y_max <- max(c(pred_grid$y, dat_df$Y)) + crop_pad
 # ---------------------------
 
 p1 <- ggplot() +
-    geom_sf(data = shore_union, fill = "grey90", colour = NA) +
-    geom_point(
-               data  = pred_grid,
-               aes(x = x, y = y),
-               size  = 0.5,
-               alpha = 0.5
-               ) +
-    coord_sf(xlim = c(x_min, x_max),
-             ylim = c(y_min, y_max),
-             expand = FALSE) +
-    labs(
-         title    = "prediction grid (original)",
-         subtitle = paste0(
-                           "grid spacing = ", grid_spacing,
-                           " m, bbox buffer = ", bbox_buffer, " m"
-         )
+  geom_sf(data = shore_union, fill = "grey90", colour = NA) +
+  geom_point(
+    data  = pred_grid,
+    aes(x = x, y = y),
+    size  = 0.5,
+    alpha = 0.5
+  ) +
+  coord_sf(
+    xlim = c(x_min, x_max),
+    ylim = c(y_min, y_max),
+    expand = FALSE
+  ) +
+  labs(
+    title = "prediction grid (original)",
+    subtitle = paste0(
+      "grid spacing = ", grid_spacing,
+      " m, bbox buffer = ", bbox_buffer, " m"
     )
+  )
 
-    p2 <- ggplot() +
-        geom_sf(data = shore_union, fill = "grey90", colour = NA) +
-        geom_point(
-                   data = dat_df,
-                   aes(x = X, y = Y),
-                   size = 1
-                   ) +
-        coord_sf(xlim = c(x_min, x_max),
-                 ylim = c(y_min, y_max),
-                 expand = FALSE) +
-        labs(title = "survey locations")
+p2 <- ggplot() +
+  geom_sf(data = shore_union, fill = "grey90", colour = NA) +
+  geom_point(
+    data = dat_df,
+    aes(x = X, y = Y),
+    size = 1
+  ) +
+  coord_sf(
+    xlim = c(x_min, x_max),
+    ylim = c(y_min, y_max),
+    expand = FALSE
+  ) +
+  labs(title = "survey locations")
 
-    # you can run: p1 + p2  # to see the originals
+# you can run: p1 + p2  # to see the originals
 
-    # ---------------------------------------------------------------------
-    # hacky way to remove that unsampled lake with poly_click
-    # ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# hacky way to remove that unsampled lake with poly_click
+# ---------------------------------------------------------------------
 
-    # work in lon/lat so axes are intuitive
-    shore_ll <- st_read("AOC_MI_StMarys_2022/AOC_MI_StMarys_2022.shp") # original NAD83
-    dat_ll   <- read.csv("all_data_through_2024/2024-10-24-CatchALL.csv")
+# work in lon/lat so axes are intuitive
+shore_ll <- st_read("AOC_MI_StMarys_2022/AOC_MI_StMarys_2022.shp") # original NAD83
+dat_ll <- read.csv("all_data_through_2024/2024-10-24-CatchALL.csv")
 
-    plot(st_geometry(shore_ll), col = "lightblue")
-    points(dat_ll$longitude, dat_ll$latitude, pch = 16, cex = 0.5)
+plot(st_geometry(shore_ll), col = "lightblue")
+points(dat_ll$longitude, dat_ll$latitude, pch = 16, cex = 0.5)
 
-    # now click around the region you want to remove, then right-click / esc to stop
-    poly_click <- locator(type = "l")   # draws as you click
+# now click around the region you want to remove, then right-click / esc to stop
+poly_click <- locator(type = "l") # draws as you click
 
-    # combine locator points into matrix (lon, lat)
-    bad_coords_ll <- cbind(poly_click$x, poly_click$y)
+# combine locator points into matrix (lon, lat)
+bad_coords_ll <- cbind(poly_click$x, poly_click$y)
 
-    # close the polygon by repeating the first point
-    bad_coords_ll <- rbind(bad_coords_ll, bad_coords_ll[1, , drop = FALSE])
+# close the polygon by repeating the first point
+bad_coords_ll <- rbind(bad_coords_ll, bad_coords_ll[1, , drop = FALSE])
 
-    bad_poly_ll <- st_sfc(
-                          st_polygon(list(bad_coords_ll)),
-                          crs = st_crs(shore_ll)  # same crs as when you clicked
-    )
+bad_poly_ll <- st_sfc(
+  st_polygon(list(bad_coords_ll)),
+  crs = st_crs(shore_ll) # same crs as when you clicked
+)
 
-    # transform bad polygon to utm 26916 (same as pred_grid)
-    bad_poly <- st_transform(bad_poly_ll, target_crs)
+# transform bad polygon to utm 26916 (same as pred_grid)
+bad_poly <- st_transform(bad_poly_ll, target_crs)
 
-    # mask pred_grid: drop cells inside bad polygon
-    grid_sf_pred <- st_as_sf(pred_grid, coords = c("x", "y"), crs = target_crs)
+# mask pred_grid: drop cells inside bad polygon
+grid_sf_pred <- st_as_sf(pred_grid, coords = c("x", "y"), crs = target_crs)
 
-    inside_bad <- st_within(grid_sf_pred, bad_poly, sparse = FALSE)[, 1]
+inside_bad <- st_within(grid_sf_pred, bad_poly, sparse = FALSE)[, 1]
 
-    grid_sf_keep     <- grid_sf_pred[!inside_bad, ]
-    grid_coords_keep <- st_coordinates(grid_sf_keep)
+grid_sf_keep <- grid_sf_pred[!inside_bad, ]
+grid_coords_keep <- st_coordinates(grid_sf_keep)
 
-    pred_grid_fix <- data.frame(
-                                x       = grid_coords_keep[, 1],
-                                y       = grid_coords_keep[, 2],
-                                area_m2 = grid_spacing^2,
-                                area_ha = (grid_spacing^2) / 10000
-    )
+pred_grid_fix <- data.frame(
+  x       = grid_coords_keep[, 1],
+  y       = grid_coords_keep[, 2],
+  area_m2 = grid_spacing^2,
+  area_ha = (grid_spacing^2) / 10000
+)
 
-    # recompute crop limits from fixed grid + points (optional but cleaner)
-    x_min_fix <- min(c(pred_grid_fix$x, dat_df$X)) - crop_pad
-    x_max_fix <- max(c(pred_grid_fix$x, dat_df$X)) + crop_pad
-    y_min_fix <- min(c(pred_grid_fix$y, dat_df$Y)) - crop_pad
-    y_max_fix <- max(c(pred_grid_fix$y, dat_df$Y)) + crop_pad
+# recompute crop limits from fixed grid + points (optional but cleaner)
+x_min_fix <- min(c(pred_grid_fix$x, dat_df$X)) - crop_pad
+x_max_fix <- max(c(pred_grid_fix$x, dat_df$X)) + crop_pad
+y_min_fix <- min(c(pred_grid_fix$y, dat_df$Y)) - crop_pad
+y_max_fix <- max(c(pred_grid_fix$y, dat_df$Y)) + crop_pad
 
-    # ---------------------------
-    # p3: fixed prediction grid
-    # ---------------------------
+# ---------------------------
+# p3: fixed prediction grid
+# ---------------------------
 
-    p3 <- ggplot() +
-        geom_sf(data = shore_union, fill = "grey90", colour = NA) +
-        geom_point(
-                   data  = pred_grid_fix,
-                   aes(x = x, y = y),
-                   size  = 0.5,
-                   alpha = 0.5
-                   ) +
-        coord_sf(xlim = c(x_min_fix, x_max_fix),
-                 ylim = c(y_min_fix, y_max_fix),
-                 expand = FALSE) +
-        labs(
-             title = "prediction grid (masked)"
-        )
+p3 <- ggplot() +
+  geom_sf(data = shore_union, fill = "grey90", colour = NA) +
+  geom_point(
+    data  = pred_grid_fix,
+    aes(x = x, y = y),
+    size  = 0.5,
+    alpha = 0.5
+  ) +
+  coord_sf(
+    xlim = c(x_min_fix, x_max_fix),
+    ylim = c(y_min_fix, y_max_fix),
+    expand = FALSE
+  ) +
+  labs(
+    title = "prediction grid (masked)"
+  )
 
-        # ---------------------------
-        # three-panel layout: original grid, surveys, fixed grid
-        # ---------------------------
+# ---------------------------
+# three-panel layout: original grid, surveys, fixed grid
+# ---------------------------
 
-        fig <- p1 + p2 + p3
-        ggsave(
-               filename = "prediction_grid_diagnostics.pdf",
-               plot     = fig,
-               width    = 14,
-               height   = 11,
-        )
+fig <- p1 + p2 + p3
+ggsave(
+  filename = "prediction_grid_diagnostics.pdf",
+  plot     = fig,
+  width    = 14,
+  height   = 11,
+)
 
-        grid_sf_out <- st_as_sf(
-                                pred_grid_fix,
-                                coords = c("x", "y"),
-                                crs    = target_crs
-        )
+grid_sf_out <- st_as_sf(
+  pred_grid_fix,
+  coords = c("x", "y"),
+  crs    = target_crs
+)
 
-        st_write(
-                 grid_sf_out,
-                 "prediction_grid_utm26916_masked.gpkg",
-                 delete_dsn = TRUE
-        )
+st_write(
+  grid_sf_out,
+  "prediction_grid_utm26916_masked.gpkg",
+  delete_dsn = TRUE
+)

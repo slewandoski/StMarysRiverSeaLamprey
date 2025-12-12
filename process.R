d86c856052ce0f39abb068736b856c791121cce8
ChrisFishCahill <cahill11@msu.edu>
2025-12-11 19:11:22 -0500

    plots

--- process.R
+++ process.R
@@ -1,39 +1,44 @@
 # start of processing script
-
+library(sdmTMB)
+library(sf)
+library(ggplot2)
+library(dplyr)
+library(raster)
+load('output/sdmTMB_models.RData')
 # general plots and diagnostics, can (should) be run for each model
-tidy(m1, conf.int = TRUE)
-tidy(m1, effects = "ran_pars", conf.int = TRUE)
+# tidy(m3, conf.int = TRUE)
+# tidy(m3, effects = "ran_pars", conf.int = TRUE)
 # plot depth effect
-ggeffects::ggpredict(m1, terms="depth[0:40, by = 2]") |> plot()
-data$resids <- residuals(m1) # randomized quantile residuals
-hist(data$resids)
-ggplot(data, aes(X, Y, col = resids)) + scale_colour_gradient2() +
-    geom_point() + facet_wrap(~year, nrow = 3) + coord_fixed()
-set.seed(19283)
-s <- simulate(m1, nsim = 1000, type = "mle-mvn")
-dharma_residuals(s, m1)
-abline(0,1)
+# ggeffects::ggpredict(m3, terms="depth[0:40, by = 2]") |> plot()
+# data$resids <- residuals(m3) # randomized quantile residuals
+# hist(data$resids)
+# ggplot(data, aes(X, Y, col = resids)) + scale_colour_gradient2() +
+#    geom_point() + facet_wrap(~year, nrow = 3) + coord_fixed()
+# set.seed(19283)
+# s <- simulate(m3, nsim = 1000, type = "mle-mvn")
+# dharma_residuals(s, m3)
+# abline(0,1)
 
-ggplot(data, aes(X, Y, col = resids)) +
-    scale_colour_gradient2() +
-    geom_point() +
-    facet_wrap(~year, nrow = 4) +
-    coord_fixed()
+# ggplot(data, aes(X, Y, col = resids)) +
+#    scale_colour_gradient2() +
+#    geom_point() +
+#    facet_wrap(~year, nrow = 4) +
+#    coord_fixed()
 
 # install.packages("remotes")
 # remotes::install_github("pbs-assess/sdmTMBextra", dependencies = TRUE)
 #set.seed(123)
-#samps <- sdmTMBextra::predict_mle_mcmc(m1, mcmc_iter = 800, mcmc_warmup = 400)
-#mcmc_res <- residuals(m1, type = "mle-mcmc", mcmc_samples = samps)
+#samps <- sdmTMBextra::predict_mle_mcmc(m3, mcmc_iter = 800, mcmc_warmup = 400)
+#mcmc_res <- residuals(m3, type = "mle-mcmc", mcmc_samples = samps)
 #qqnorm(mcmc_res)
 #abline(0, 1)
 
 # simulation based stuff - number of zeros
-s_nb2 <- simulate(m1, nsim = 500, type = "mle-mvn")
+s_nb2 <- simulate(m3, nsim = 500, type = "mle-mvn")
 sum(data$c == 0) / length(data$c) # observed
 sum(s_nb2 == 0)/length(s_nb2) # predicted
 
-r_nbinom <- dharma_residuals(s_nb2, m1, return_DHARMa = TRUE)
+r_nbinom <- dharma_residuals(s_nb2, m3, return_DHARMa = TRUE)
 plot(r_nbinom)
 DHARMa::testResiduals(r_nbinom)
 
@@ -45,7 +50,7 @@ data$year_fac <- as.factor(data$year)
 grid_yrs <- replicate_df(pred_grid, "year", unique(data$year))
 grid_yrs$year_fac <- as.factor(grid_yrs$year)
 
-predictions <- predict(m2, newdata = grid_yrs, return_tmb_object = TRUE)
+predictions <- predict(m3, newdata = grid_yrs, return_tmb_object = TRUE)
 
 # function to make maps
 plot_map <- function(dat, column) {
@@ -78,7 +83,7 @@ p5 <- ggplot(index, aes(year, est)) + geom_line() +
     xlab('Year') + ylab('Number of juvenile lamprey')
 
 # multi-page pdf"
-pdf("ar1st_idx_std.pdf", width = 15, height = 11)
+pdf("plots/ar1st_idx_std.pdf", width = 15, height = 11)
 print(p1)
 print(p2)
 print(p3)
